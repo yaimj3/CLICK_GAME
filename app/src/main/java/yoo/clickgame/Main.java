37c1d340ce7fe94990cf07d186e87b20e27e8bff
yaimj3 <yaimj3@gmail.com>
2015-12-22 02:23:19 -0600

    Background added
    Fixed clicksound
    Added background
    Still need to check this app is working on fullscreen

--- app/src/main/java/yoo/clickgame/Main.java
+++ app/src/main/java/yoo/clickgame/Main.java
@@ -12,203 +12,183 @@ public class Main extends ActionBarActivity {
     private TextView score;
     private ImageButton image;
     private MediaPlayer backgroundMusic,clickSound;
-
-
-    int click=0;
+    int click = 0;
     int setNum = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
 
         backgroundMusic = MediaPlayer.create(Main.this, R.raw.background);
-        clickSound = MediaPlayer.create(Main.this,R.raw.click);
+        backgroundMusic.setLooping(true);
+        backgroundMusic.setVolume(0.1f, 0.1f);
+
+        clickSound = MediaPlayer.create(Main.this, R.raw.click);
         score = (TextView) findViewById(R.id.scoreText);
         image = (ImageButton) findViewById(R.id.ImageButton);
 
 
-        backgroundMusic.setLooping(true);
-        backgroundMusic.setVolume(0.7f, 0.7f);
         backgroundMusic.start();
 
+        image.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clickSound.setVolume(1.0f, 1.0f);
+                clickSound.start();
+                clickSound.stop();
+
+                click++;
+                checksetNum(v);
+
+            }
+        });
     }
 
-    public void clickImage(View view)
-    {
-        click++;
-        clickSound.start();
-
-        if (click==setNum)
-        {
-            click=0;
-            setNum =setNum+5;
-
-
-            String temp = click+" / "+setNum;
-            score.setText(temp);
-
-
-            changeImage(view, setNum);
-
-        }
-        else {
-
-            String temp = click + " / " + setNum;
-            score.setText(temp);
-        }
-    }
 
     @Override
-    public void onPause()
-    {
+    public void onPause() {
         super.onPause();
 
 
-        backgroundMusic.pause();
+      backgroundMusic.pause();
     }
 
     @Override
     public void onStop() {
         super.onStop();
 
-        backgroundMusic.stop();
+      backgroundMusic.stop();
     }
+
     @Override
     public void onResume() {
         super.onResume();
 
 
-        backgroundMusic.start();
+      backgroundMusic.start();
     }
 
     @Override
-    public void onRestart()
-    {
+    public void onRestart() {
         super.onRestart();
-        backgroundMusic.start();
+      backgroundMusic.start();
     }
 
     @Override
-    public void onDestroy()
-    {
+    public void onDestroy() {
         super.onDestroy();
-        backgroundMusic.stop();
+      backgroundMusic.stop();
     }
 
-    public void changeImage(View view, int setNum){
+    public void checksetNum(View view)
+    {
+        if (click == setNum) {
+            click = 0;
+            setNum = setNum + 5;
 
-        switch(setNum)
-        {
-            case 10:
-            {
+
+            String temp = click + " / " + setNum;
+            score.setText(temp);
+            changeImage(view,setNum);
+
+
+        } else {
+
+            String temp = click + " / " + setNum;
+            score.setText(temp);
+        }
+    }
+    public void changeImage(View view, int setNum)
+    {
+        switch (setNum) {
+            case 10: {
                 image.setImageResource(R.drawable.two);
                 break;
             }
-            case 15:
-            {
+            case 15: {
                 image.setImageResource(R.drawable.three);
                 break;
             }
-            case 20:
-            {
+            case 20: {
                 image.setImageResource(R.drawable.four);
                 break;
             }
-            case 25:
-            {
+            case 25: {
                 image.setImageResource(R.drawable.five);
                 break;
             }
-            case 30:
-            {
+            case 30: {
                 image.setImageResource(R.drawable.six);
                 break;
             }
-            case 35:
-            {
+            case 35: {
                 image.setImageResource(R.drawable.seven);
                 break;
             }
-            case 40:
-            {
+            case 40: {
                 image.setImageResource(R.drawable.eight);
                 break;
             }
-            case 45:
-            {
+            case 45: {
                 image.setImageResource(R.drawable.nine);
                 break;
             }
-            case 50:
-            {
+            case 50: {
                 image.setImageResource(R.drawable.ten);
                 break;
             }
-            case 55:
-            {
+            case 55: {
                 image.setImageResource(R.drawable.eleven);
                 break;
             }
-            case 60:
-            {
+            case 60: {
                 image.setImageResource(R.drawable.twelve);
                 break;
             }
-            case 65:
-            {
+            case 65: {
                 image.setImageResource(R.drawable.thirteen);
                 break;
             }
-            case 70:
-            {
+            case 70: {
                 image.setImageResource(R.drawable.fourteen);
                 break;
             }
-            case 75:
-            {
+            case 75: {
                 image.setImageResource(R.drawable.fifteen);
                 break;
             }
-            case 80:
-            {
+            case 80: {
                 image.setImageResource(R.drawable.sixteen);
                 break;
             }
-            case 85:
-            {
+            case 85: {
                 image.setImageResource(R.drawable.seventeen);
                 break;
             }
-            case 90:
-            {
+            case 90: {
                 image.setImageResource(R.drawable.eighteen);
                 break;
             }
-            case 95:
-            {
+            case 95: {
                 image.setImageResource(R.drawable.nineteen);
                 break;
             }
 
-            case 100:
-            {
+            case 100: {
                 image.setImageResource(R.drawable.twenty);
                 break;
             }
-            case 105:
-            {
+            case 105: {
                 image.setImageResource(R.drawable.twentyone);
                 break;
             }
-            default:
-            {
+            default: {
                 break;
             }
 
 
         }
-
     }
-
 }
